45c372e2fb997eae6919d2654116f761c69cf1ec
Siviwe Majova <201229479@student.uj.ac.za>
2018-11-13 21:56:44 +0200

    Add angular app module with controllers

---
+++ frontend/js/app.js
@@ -0,0 +1,10 @@
+var app = angular.module('boreholeManager', []);
+
+app.controller('BoreholeList', ['$scope', '$http', function($scope, $http) {
+
+}]);
+
+app.controller('AddBorehole', ['$scope', '$http', function($scope, $http) {
+
+}]);
+
