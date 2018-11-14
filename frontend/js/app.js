701146265cde6e366d0188090d2d9215960a314f
Siviwe Majova <201229479@student.uj.ac.za>
2018-11-14 17:41:36 +0200

    Add borehole list partial

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,10 +1,20 @@
-var app = angular.module('boreholeManager', []);
+var app = angular.module('boreholeManager', ['ngRoute']);
 
-app.controller('BoreholeList', ['$scope', '$http', function($scope, $http) {
+app.config(function($routeProvider) {
+    $routeProvider
+    .when('/', {
+        templateUrl:'partials/list.htm',
+        controller: 'boreholeList'
+    });
+});
+
+app.controller('boreholeList', ['$scope', '$http', function($scope, $http) {
+
+    $scope.list = [{name: 'Borehole1'}, {name: 'Borehole2'}];
 
 }]);
 
-app.controller('AddBorehole', ['$scope', '$http', function($scope, $http) {
+app.controller('addBorehole', ['$scope', '$http', function($scope, $http) {
 
 }]);
 
